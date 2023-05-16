d4d37b8ed812987a8ffb4e86941fff7e5c756ba2
SamuelKruzliak <99512834+SamuelKruzliak@users.noreply.github.com>
2023-05-16 19:39:42 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,120 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace networking
+namespace VLSM_final_version
 {
-    class Program
+    class Program:IpAddress
     {
         static void Main(string[] args)
         {
-            List<byte> numbers = new List<byte>();
-            List<int> hosts = new List<int>();
-            List<string> results = new List<string>();
-
-            Console.Write("Zadaj Ip adresu s prefixom: ");
-            string ipAdress = Console.ReadLine();
-            Console.Write("Zadaj pocet hostov: ");
-            string host_string = Console.ReadLine();
-            try
-            {
-                hosts = NumberParser.parseHosts(host_string);
-                numbers = NumberParser.ParseIpAdress(ipAdress);
-                hosts = IpAdressCalc.getPrefixList(hosts);
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine("Subnety: ");
-
-                results = IpAdressCalc.printAdresses(hosts, numbers);
-            }
-            catch
-            {
-                Console.WriteLine("Zadaj platne hodnoty!");
-            }
-
-            foreach (string item in results)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadLine();
+           UserInput[0] = "172.168.0.0/16"; //User input[0] is for Ip address
+           UserInput[1] = "1000,700,500,200,100,50,20,2"; //User input[1] is for hosts
+           InputParser.parseInput(); //Parses both inputs into parts
+           SubnetMaker.getPrefixList();
+           SubnetMaker.printAdresses();
+           foreach(string item in Subnets){
+            System.Console.WriteLine(item);
+           }
         }
     }
-
-
-    class NumberParser
+    class IpAddress
     {
-        static public List<byte> ParseIpAdress(string ipAdress)
-        {
-            List<byte> list = new List<byte>();
-            const int NumberOfIpAdressParts = 4;
-
-            list.Add(Convert.ToByte(ipAdress.Substring(ipAdress.IndexOf("/") + 1)));
-            ipAdress = ipAdress.Substring(0, ipAdress.IndexOf("/"));
-
-            for (int i = 0; i < NumberOfIpAdressParts; i++)
-            {
-                if (ipAdress.IndexOf(".") == -1)
-                {
-                    list.Add(Convert.ToByte(ipAdress));
-                }
-                else if (ipAdress.IndexOf(".") != -1)
-                {
-                    list.Add(Convert.ToByte(ipAdress.Substring(0, ipAdress.IndexOf("."))));
-                    ipAdress = ipAdress.Substring(ipAdress.IndexOf(".") + 1, ipAdress.Length - ipAdress.IndexOf(".") - 1);
-                }
-            }
-
-            return list;
-        }
-
-        static public List<int> parseHosts(string s)
-        {
-            List<int> hosts = new List<int>();
-
-
-            for (int i = 0; true; i++)
-            {
-                if (s[i] == ',')
-                {
-                    hosts.Add(int.Parse(s.Substring(0, i)));
-                    s = s.Substring(i + 1, s.Length - i - 1);
-                    i = 0;
-                }
-                if (s.IndexOf(",") == -1)
-                {
-                    hosts.Add(int.Parse(s));
-                    return hosts;
-                }
-            }
-
-        }
-
-        static List<int> arrangeList(List<int> list)
-        {
-            List<int> newList = new List<int>();
-
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                int val = 0;
-                int pos = 0;
-
-                for (int j = 0; j < list.Count; j++)
-                {
-                    if (list[j] > val)
-                    {
-                        val = list[j];
-                        pos = j;
-                    }
-                }
-                list[pos] = 0;
-
-                newList.Add(val);
-            }
-
-            return newList;
+        static public string[] UserInput = new string[2];
+        static public int[] IpAddressParts;
+        static public int[] Hosts;
+        
+        static public List<string> Subnets = new List<string>();
+    }
+    class InputParser:IpAddress
+    {
+        static public void parseInput(){
+            char[] separators = new char[] {'.', '/'};
+            IpAddressParts = Array.ConvertAll(UserInput[0].Split(separators), s => int.Parse(s));
+            Hosts = Array.ConvertAll(UserInput[1].Split(','), s => int.Parse(s));
         }
     }
-    class IpAdressCalc
+    class SubnetMaker:IpAddress
     {
+        static public int[] Prefixes = new int[Hosts.Length];
+        static public void getPrefixList(){
+            for(int i = 0; i < Hosts.Length; i++){
+                Prefixes[i] = getPrefix(Hosts[i]);
+            }
+        }
+        static public void printAdresses()
+        {
+            int add_value = 0;
+            int selected_part = 4;
+            int num;
+
+            for (int i = 0; i < Prefixes.Length; i++)
+            {
+                selected_part = calculatePart(Prefixes[i])[0];
+                num = calculatePart(Prefixes[i])[1];
+                Subnets.Add($"{IpAddressParts[0]}.{IpAddressParts[1]}.{IpAddressParts[2]}.{IpAddressParts[3]} /{Prefixes[i]} Subnet mask: {getSubnetMask(Prefixes[i])}");
+                add_value = Convert.ToInt32(Math.Pow(2, num));
+
+                if (add_value + IpAddressParts[selected_part] > 255)
+                {
+                    IpAddressParts[selected_part - 1]++;
+                    add_value = 0;
+                    IpAddressParts[selected_part] = 0;
+                }
+                IpAddressParts[selected_part] += add_value;
+
+            }
+        }
         static string getSubnetMask(int prefix)
         {
             string subnet_mask = null;
@@ -153,98 +102,44 @@ namespace networking
             }
             return subnet_mask;
         }
-
-        static public List<string> printAdresses(List<int> prefixes, List<byte> numbers)
-        {
-            List<string> ipAdresses = new List<string>();
-
-            int add_value = 0;
-            int selected_part = 4;
-            int num;
-
-            for (int i = 0; i < prefixes.Count; i++)
-            {
-                selected_part = calculatePart(prefixes[i])[0];
-                num = calculatePart(prefixes[i])[1];
-                ipAdresses.Add($"{numbers[1]}.{numbers[2]}.{numbers[3]}.{numbers[4]} /{prefixes[i]} Subnet mask: {getSubnetMask(prefixes[i])}");
-                add_value = getValue(num);
-
-                if (add_value + numbers[selected_part] > 255)
-                {
-                    numbers[selected_part - 1]++;
-                    add_value = 0;
-                    numbers[selected_part] = 0;
-                }
-                numbers[selected_part] += Convert.ToByte(add_value);
-
-            }
-
-            return ipAdresses;
-        }
-
         static int[] calculatePart(int prefix)
         {
             int[] res = new int[2];
             if (prefix < 8)
             {
-                res[0] = 1;
+                res[0] = 0;
                 res[1] = 8 - prefix;
 
             }
             else if (prefix < 16)
             {
-                res[0] = 2;
+                res[0] = 1;
                 res[1] = 8 - (prefix - 8);
 
             }
             else if (prefix < 24)
             {
-                res[0] = 3;
+                res[0] = 2;
                 res[1] = 8 - (prefix - 16);
             }
             else
             {
 
-                res[0] = 4;
+                res[0] = 3;
                 res[1] = 32 - prefix;
             }
             return res;
         }
-
-        static int getValue(int num)
-        {
-            return Convert.ToInt32(Math.Pow(2, num));
-        }
-
-        static public List<int> getPrefixList(List<int> hosts)
-        {
-            List<int> prefixes = new List<int>();
-
-            for (int i = 0; i < hosts.Count; i++)
-            {
-                prefixes.Add(getPrefix(hosts[i]));
-            }
-
-            return prefixes;
-        }
-
         static int getPrefix(int hostNum)
         {
-            int prefix = 0;
-            int powNum = 0;
-
             for (int i = 0; true; i++)
             {
-                powNum = i;
-
-                if (Math.Pow(2, powNum) >= hostNum + 2)
+                if (Math.Pow(2, i) >= hostNum + 2)
                 {
-                    prefix = 32 - powNum;
-                    break;
+                    return 32 - i;
                 }
             }
-
-            return prefix;
         }
+
     }
 }
